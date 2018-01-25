ee2d86abdab18adefff32350197ad1f09af81e10
Florin Alexandrescu <florin.alexandrescu@mapquest.com>
2018-01-25 11:29:01 -0700

    Make it work in Safari

--- js/Wave.js
+++ js/Wave.js
@@ -3,7 +3,7 @@ class Wave {
     this.file = file;
 
     if(!window.audioContext) {
-      window.audioContext = new AudioContext();
+      window.audioContext = new (window.AudioContext || window.webkitAudioContext)();  //Safar needs the webkitAudioContext part
     }
     this.audioContext = window.audioContext;
     this.gainNode = this.audioContext.createGain();
@@ -16,17 +16,16 @@ class Wave {
     request.responseType = 'arraybuffer';
 
     request.onload = () => {
-      this.audioContext.decodeAudioData(request.response)
-        .then(decodedData => {
-          this.source.buffer = decodedData;
+      this.audioContext.decodeAudioData(request.response, decodedData =>{ //use a callback bc Safari 11.0.2 doesn't support promises here
+        this.source.buffer = decodedData;
 
-          this.source.connect(this.gainNode);
-          this.gainNode.connect(this.audioContext.destination);
-          this.gainNode.gain.exponentialRampToValueAtTime(0.01, this.audioContext.currentTime + 3); //fade out
+        this.source.connect(this.gainNode);
+        this.gainNode.connect(this.audioContext.destination);
+        this.gainNode.gain.exponentialRampToValueAtTime(0.01, this.audioContext.currentTime + 3); //fade out
 
-          this.source.start(0);
-        });
-      };
+        this.source.start(0, 0, 3); //same as fadeout
+      });
+    };
 
     request.send();
   }
